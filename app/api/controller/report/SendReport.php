73735b4568102013e1c48a4e13d0037f2edda52e
chenweiliang <512430@qq.com>
2023-05-20 16:09:27 +0800

    111

--- app/api/controller/report/SendReport.php
+++ app/api/controller/report/SendReport.php
@@ -86,21 +86,23 @@ class SendReport extends BaseController
         } elseif ($name =='S043') {
             $this->service->create_table_s043();
         } elseif ($name =='S111' || $name = 'S112') {
-            $res = http_get('http://www.easyadmin1.com/api/Tableupdate/receipt_receiptNotice');
+            $res = http_get('http://im.babiboy.com//api/Tableupdate/receipt_receiptNotice');
+            // $res = http_get('http://www.easyadmin1.com/api/Tableupdate/receipt_receiptNotice');
             $res =json_decode($res, true);
             if ($res['status'] == 1) {
-                $this->service->create_table_s111('春季');
-                $this->service->create_table_s111('夏季');
-                $this->service->create_table_s111('秋季');
-                $this->service->create_table_s111('冬季');
+            $this->service->create_table_s111('春季');
+            $this->service->create_table_s111('夏季');
+            $this->service->create_table_s111('秋季');
+            $this->service->create_table_s111('冬季');
 
-                $this->service->create_table_s112('春季');
-                $this->service->create_table_s112('夏季');
-                $this->service->create_table_s112('秋季');
-                $this->service->create_table_s112('冬季');
+            $this->service->create_table_s112('春季');
+            $this->service->create_table_s112('夏季');
+            $this->service->create_table_s112('秋季');
+            $this->service->create_table_s112('冬季');
             } else {
                 return $res;
             }
+
         } elseif ($name =='S112') {
             $this->service->create_table_s112('春季');
             $this->service->create_table_s112('夏季');
