6ce571253bc88cff03680d08710574c0be0cee14
wmy <1664747641@qq.com>
2023-05-30 20:40:03 +0800

    提交模型

---
+++ app/admin/model/code/SizeWarehouseTransitStock.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\admin\model\code;
+
+
+use app\common\model\TimeModel;
+
+class SizeWarehouseTransitStock extends TimeModel
+{
+    // 表名
+    protected $name = 'size_warehouse_transit_stock';
+
+    /**
+     * 获取尺码字段
+     */
+    public static function getSizeKey($key)
+    {
+        // 总尺码
+        $key_arr = [
+            '云仓库存_00/28/37/44/100/160/S',
+            '云仓库存_29/38/46/105/165/M',
+            '云仓库存_30/39/48/110/170/L',
+            '云仓库存_31/40/50/115/175/XL',
+            '云仓库存_32/41/52/120/180/2XL',
+            '云仓库存_33/42/54/125/185/3XL',
+            '云仓库存_34/43/56/190/4XL',
+            '云仓库存_35/44/58/195/5XL',
+            '云仓库存_36/6XL',
+            '云仓库存_38/7XL',
+            '云仓库存_40/8XL'
+        ];
+        // 匹配尺码
+        foreach ($key_arr as $k => $v){
+            if(strpos($v,$key) !== false){
+                return $v;
+            }
+        }
+        return $key;
+    }
+}
