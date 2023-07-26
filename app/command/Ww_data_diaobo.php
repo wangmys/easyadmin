a79fa263711d21ba9aaf7f4ec0fb61f96948f3bd
lyp <1606038679@qq.com>
2023-07-26 19:23:14 +0800

    ww调拨数据

--- app/command/Ww_data_diaobo.php
+++ app/command/Ww_data_diaobo.php
@@ -53,40 +53,37 @@ class Ww_data_diaobo extends Command
         $sql= "WITH T1 AS 
         (
         SELECT 
-            ECR.CustomerId,
-            ECRG.GoodsId,
-            SUM(CASE WHEN EBGS.ViewOrder=1  THEN ECRGD.Quantity END ) 	[调入数量_00/28/37/44/100/160/S],
-          SUM(CASE WHEN EBGS.ViewOrder=2  THEN ECRGD.Quantity END ) 	[调入数量_29/38/46/105/165/M],
-          SUM(CASE WHEN EBGS.ViewOrder=3  THEN ECRGD.Quantity END ) 	[调入数量_30/39/48/110/170/L],
-          SUM(CASE WHEN EBGS.ViewOrder=4  THEN ECRGD.Quantity END ) 	[调入数量_31/40/50/115/175/XL],
-          SUM(CASE WHEN EBGS.ViewOrder=5  THEN ECRGD.Quantity END ) 	[调入数量_32/41/52/120/180/2XL],
-          SUM(CASE WHEN EBGS.ViewOrder=6  THEN ECRGD.Quantity END ) 	[调入数量_33/42/54/125/185/3XL],
-          SUM(CASE WHEN EBGS.ViewOrder=7  THEN ECRGD.Quantity END ) 	[调入数量_34/43/56/190/4XL],
-          SUM(CASE WHEN EBGS.ViewOrder=8  THEN ECRGD.Quantity END ) 	[调入数量_35/44/58/195/5XL],
-          SUM(CASE WHEN EBGS.ViewOrder=9  THEN ECRGD.Quantity END ) 	[调入数量_36/6XL],
-          SUM(CASE WHEN EBGS.ViewOrder=10 THEN ECRGD.Quantity END ) 	[调入数量_38/7XL],
-          SUM(CASE WHEN EBGS.ViewOrder=11 THEN ECRGD.Quantity END ) 	[调入数量_40],
-            SUM(ECRGD.Quantity) AS 调入总数量
-        FROM ErpCustReceipt ECR
-        LEFT JOIN ErpCustReceiptGoods ECRG  ON ECR.ReceiptID=ECRG.ReceiptID
-        LEFT JOIN ErpCustReceiptGoodsDetail ECRGD ON ECRG.ReceiptGoodsID=ECRGD.ReceiptGoodsID
-        LEFT JOIN ErpBaseGoodsSize EBGS ON ECRGD.SizeId=EBGS.SizeId
-        LEFT JOIN ErpCustOutbound ECO ON ECRG.CustOutboundId=ECO.CustOutboundId
-        WHERE CONVERT(VARCHAR(10),ECR.ReceiptDate,23)  BETWEEN '{$start_date}' AND '{$end_date}'
-            AND ECR.Type=2
-            AND ECO.ManualNo IS NOT NULL 
-            AND ECO.ManualNo!=''
+          EIA.InItemId CustomerId,
+          EIAG.GoodsId,
+          SUM(CASE WHEN EBGS.ViewOrder=1  THEN EIAGD.Quantity END ) 	[调入数量_00/28/37/44/100/160/S],
+          SUM(CASE WHEN EBGS.ViewOrder=2  THEN EIAGD.Quantity END ) 	[调入数量_29/38/46/105/165/M],
+          SUM(CASE WHEN EBGS.ViewOrder=3  THEN EIAGD.Quantity END ) 	[调入数量_30/39/48/110/170/L],
+          SUM(CASE WHEN EBGS.ViewOrder=4  THEN EIAGD.Quantity END ) 	[调入数量_31/40/50/115/175/XL],
+          SUM(CASE WHEN EBGS.ViewOrder=5  THEN EIAGD.Quantity END ) 	[调入数量_32/41/52/120/180/2XL],
+          SUM(CASE WHEN EBGS.ViewOrder=6  THEN EIAGD.Quantity END ) 	[调入数量_33/42/54/125/185/3XL],
+          SUM(CASE WHEN EBGS.ViewOrder=7  THEN EIAGD.Quantity END ) 	[调入数量_34/43/56/190/4XL],
+          SUM(CASE WHEN EBGS.ViewOrder=8  THEN EIAGD.Quantity END ) 	[调入数量_35/44/58/195/5XL],
+          SUM(CASE WHEN EBGS.ViewOrder=9  THEN EIAGD.Quantity END ) 	[调入数量_36/6XL],
+          SUM(CASE WHEN EBGS.ViewOrder=10 THEN EIAGD.Quantity END ) 	[调入数量_38/7XL],
+          SUM(CASE WHEN EBGS.ViewOrder=11 THEN EIAGD.Quantity END ) 	[调入数量_40],
+          SUM(EIAGD.Quantity) AS 调入总数量
+        FROM ErpInstructionApply EIA
+        LEFT JOIN ErpInstructionApplyGoods EIAG ON EIA.InstructionApplyId = EIAG.InstructionApplyId
+        LEFT JOIN ErpInstructionApplyGoodsDetail EIAGD ON EIAG.InstructionApplyGoodsId = EIAGD.InstructionApplyGoodsId
+        LEFT JOIN ErpBaseGoodsSize EBGS ON EIAGD.SizeId=EBGS.SizeId
+        WHERE CONVERT(VARCHAR,EIA.InstructionApplyDate,23) BETWEEN '{$start_date}'/*开始日期时间参数*/ AND '{$end_date}'/*结束日期时间参数*/
+          AND EIA.CodingCodeText='已审结'
         GROUP BY
-            ECR.CustomerId,
-            ECRG.GoodsId
+          EIA.InItemId,
+          EIAG.GoodsId
         ),
         
         T2 AS 
         (
         SELECT 
-            ECS.CustomerId,
-            ECS.GoodsId,
-            SUM(CASE WHEN EBGS.ViewOrder=1  THEN ECSD.Quantity END ) 	[调入前库存_00/28/37/44/100/160/S],
+          ECS.CustomerId,
+          ECS.GoodsId,
+          SUM(CASE WHEN EBGS.ViewOrder=1  THEN ECSD.Quantity END ) 	[调入前库存_00/28/37/44/100/160/S],
           SUM(CASE WHEN EBGS.ViewOrder=2  THEN ECSD.Quantity END ) 	[调入前库存_29/38/46/105/165/M],
           SUM(CASE WHEN EBGS.ViewOrder=3  THEN ECSD.Quantity END ) 	[调入前库存_30/39/48/110/170/L],
           SUM(CASE WHEN EBGS.ViewOrder=4  THEN ECSD.Quantity END ) 	[调入前库存_31/40/50/115/175/XL],
@@ -97,25 +94,25 @@ class Ww_data_diaobo extends Command
           SUM(CASE WHEN EBGS.ViewOrder=9  THEN ECSD.Quantity END ) 	[调入前库存_36/6XL],
           SUM(CASE WHEN EBGS.ViewOrder=10 THEN ECSD.Quantity END ) 	[调入前库存_38/7XL],
           SUM(CASE WHEN EBGS.ViewOrder=11 THEN ECSD.Quantity END ) 	[调入前库存_40],
-            SUM(ECSD.Quantity) AS 调入前库存,
-            DATEDIFF(DAY,MIN(ECS.StockDate),GETDATE()) 上市天数
+          SUM(ECSD.Quantity) AS 调入前库存,
+          DATEDIFF(DAY,MIN(ECS.StockDate),GETDATE()) 上市天数
         FROM ErpCustomerStock ECS
         LEFT JOIN ErpCustomerStockDetail ECSD ON ECS.StockId=ECSD.StockId
         LEFT JOIN ErpBaseGoodsSize EBGS ON ECSD.SizeId=EBGS.SizeId
         JOIN T1 ON T1.CustomerId = ECS.CustomerId AND T1.GoodsId= ECS.GoodsId
         WHERE CONVERT(VARCHAR(10),ECS.StockDate,23) < '{$start_date}'
         GROUP BY 
-            ECS.CustomerId,
-            ECS.GoodsId
+          ECS.CustomerId,
+          ECS.GoodsId
         HAVING SUM(ECSD.Quantity)!=0
         ),
         
         T3 AS 
         (
         SELECT 
-            A.CustomerId,
-            A.GoodsId,
-            SUM(A.[调入前库存_00/28/37/44/100/160/S]) [调入后库存_00/28/37/44/100/160/S],
+          A.CustomerId,
+          A.GoodsId,
+          SUM(A.[调入前库存_00/28/37/44/100/160/S]) [调入后库存_00/28/37/44/100/160/S],
           SUM(A.[调入前库存_29/38/46/105/165/M]) 		[调入后库存_29/38/46/105/165/M],
           SUM(A.[调入前库存_30/39/48/110/170/L]) 		[调入后库存_30/39/48/110/170/L],
           SUM(A.[调入前库存_31/40/50/115/175/XL])		[调入后库存_31/40/50/115/175/XL],
@@ -126,13 +123,13 @@ class Ww_data_diaobo extends Command
           SUM(A.[调入前库存_36/6XL])								[调入后库存_36/6XL],
           SUM(A.[调入前库存_38/7XL])								[调入后库存_38/7XL],
           SUM(A.[调入前库存_40])										[调入后库存_40],
-            SUM(A.调入前库存)													调入后库存
+          SUM(A.调入前库存)													调入后库存
         FROM 
         (
         SELECT 
-            T2.CustomerId,
-            T2.GoodsId,
-            T2.[调入前库存_00/28/37/44/100/160/S],
+          T2.CustomerId,
+          T2.GoodsId,
+          T2.[调入前库存_00/28/37/44/100/160/S],
           T2.[调入前库存_29/38/46/105/165/M],
           T2.[调入前库存_30/39/48/110/170/L],
           T2.[调入前库存_31/40/50/115/175/XL],
@@ -143,13 +140,13 @@ class Ww_data_diaobo extends Command
           T2.[调入前库存_36/6XL],
           T2.[调入前库存_38/7XL],
           T2.[调入前库存_40],
-            T2.调入前库存
+          T2.调入前库存
         FROM T2
         UNION ALL 
         SELECT 
-            T1.CustomerId,
-            T1.GoodsId,
-            T1.[调入数量_00/28/37/44/100/160/S],
+          T1.CustomerId,
+          T1.GoodsId,
+          T1.[调入数量_00/28/37/44/100/160/S],
           T1.[调入数量_29/38/46/105/165/M],
           T1.[调入数量_30/39/48/110/170/L],
           T1.[调入数量_31/40/50/115/175/XL],
@@ -160,21 +157,47 @@ class Ww_data_diaobo extends Command
           T1.[调入数量_36/6XL],
           T1.[调入数量_38/7XL],
           T1.[调入数量_40],
-            T1.调入总数量
+          T1.调入总数量
         FROM T1
+        
+        UNION ALL 
+        SELECT 
+          ECR.CustomerId,
+          ECRG.GoodsId,
+          SUM(CASE WHEN EBGS.ViewOrder=1  THEN ECRGD.Quantity END ) 	[收仓库数量_00/28/37/44/100/160/S],
+          SUM(CASE WHEN EBGS.ViewOrder=2  THEN ECRGD.Quantity END ) 	[收仓库数量_29/38/46/105/165/M],
+          SUM(CASE WHEN EBGS.ViewOrder=3  THEN ECRGD.Quantity END ) 	[收仓库数量_30/39/48/110/170/L],
+          SUM(CASE WHEN EBGS.ViewOrder=4  THEN ECRGD.Quantity END ) 	[收仓库数量_31/40/50/115/175/XL],
+          SUM(CASE WHEN EBGS.ViewOrder=5  THEN ECRGD.Quantity END ) 	[收仓库数量_32/41/52/120/180/2XL],
+          SUM(CASE WHEN EBGS.ViewOrder=6  THEN ECRGD.Quantity END ) 	[收仓库数量_33/42/54/125/185/3XL],
+          SUM(CASE WHEN EBGS.ViewOrder=7  THEN ECRGD.Quantity END ) 	[收仓库数量_34/43/56/190/4XL],
+          SUM(CASE WHEN EBGS.ViewOrder=8  THEN ECRGD.Quantity END ) 	[收仓库数量_35/44/58/195/5XL],
+          SUM(CASE WHEN EBGS.ViewOrder=9  THEN ECRGD.Quantity END ) 	[收仓库数量_36/6XL],
+          SUM(CASE WHEN EBGS.ViewOrder=10 THEN ECRGD.Quantity END ) 	[收仓库数量_38/7XL],
+          SUM(CASE WHEN EBGS.ViewOrder=11 THEN ECRGD.Quantity END ) 	[收仓库数量_40],
+          SUM(ECRGD.Quantity) AS 收仓库总数量
+        FROM ErpCustReceipt ECR
+        LEFT JOIN ErpCustReceiptGoods ECRG  ON ECR.ReceiptID=ECRG.ReceiptID
+        LEFT JOIN ErpCustReceiptGoodsDetail ECRGD ON ECRG.ReceiptGoodsID=ECRGD.ReceiptGoodsID
+        LEFT JOIN ErpBaseGoodsSize EBGS ON ECRGD.SizeId=EBGS.SizeId
+        WHERE CONVERT(VARCHAR(10),ECR.ReceiptDate,23)  BETWEEN '{$start_date}' AND '{$end_date}'
+          AND ECR.Type=1
+        GROUP BY
+          ECR.CustomerId,
+          ECRG.GoodsId
         ) A
         GROUP BY 
-            A.CustomerId,
-            A.GoodsId
+          A.CustomerId,
+          A.GoodsId
         ),
         
         
         T4 AS 
         (
         SELECT 
-            ER.CustomerId,
-            ERG.GoodsId,
-            SUM(CASE WHEN  CONVERT(VARCHAR(10),ER.RetailDate,23) BETWEEN DATEADD(DAY, -14, '{$start_date}') AND DATEADD(DAY, -1, '{$start_date}') AND EBGS.ViewOrder=1 THEN ERGD.Quantity END ) 	[调拨前两周销量_00/28/37/44/100/160/S],
+          ER.CustomerId,
+          ERG.GoodsId,
+          SUM(CASE WHEN  CONVERT(VARCHAR(10),ER.RetailDate,23) BETWEEN DATEADD(DAY, -14, '{$start_date}') AND DATEADD(DAY, -1, '{$start_date}') AND EBGS.ViewOrder=1 THEN ERGD.Quantity END ) 	[调拨前两周销量_00/28/37/44/100/160/S],
           SUM(CASE WHEN  CONVERT(VARCHAR(10),ER.RetailDate,23) BETWEEN DATEADD(DAY, -14, '{$start_date}') AND DATEADD(DAY, -1, '{$start_date}') AND EBGS.ViewOrder=2 THEN ERGD.Quantity END ) 	[调拨前两周销量_29/38/46/105/165/M],
           SUM(CASE WHEN  CONVERT(VARCHAR(10),ER.RetailDate,23) BETWEEN DATEADD(DAY, -14, '{$start_date}') AND DATEADD(DAY, -1, '{$start_date}') AND EBGS.ViewOrder=3 THEN ERGD.Quantity END ) 	[调拨前两周销量_30/39/48/110/170/L],
           SUM(CASE WHEN  CONVERT(VARCHAR(10),ER.RetailDate,23) BETWEEN DATEADD(DAY, -14, '{$start_date}') AND DATEADD(DAY, -1, '{$start_date}') AND EBGS.ViewOrder=4 THEN ERGD.Quantity END ) 	[调拨前两周销量_31/40/50/115/175/XL],
@@ -211,30 +234,30 @@ class Ww_data_diaobo extends Command
           SUM(CASE WHEN CONVERT(VARCHAR(10),ER.RetailDate,23) BETWEEN DATEADD(DAY, -13, '{$end_date}') AND DATEADD(DAY, 0, '{$end_date}') AND EBGS.ViewOrder=10 THEN ERGD.Quantity END ) 	[调拨后两周销量_38/7XL],
           SUM(CASE WHEN CONVERT(VARCHAR(10),ER.RetailDate,23) BETWEEN DATEADD(DAY, -13, '{$end_date}') AND DATEADD(DAY, 0, '{$end_date}') AND EBGS.ViewOrder=11 THEN ERGD.Quantity END ) 	[调拨后两周销量_40],
           SUM(CASE WHEN CONVERT(VARCHAR(10),ER.RetailDate,23) BETWEEN DATEADD(DAY, -13, '{$end_date}') AND DATEADD(DAY, 0, '{$end_date}') THEN ERGD.Quantity END ) 调拨后两周总销量,
-            DATEDIFF(DAY, MIN(ER.RetailDate), GETDATE()) 最早销售时间
+          DATEDIFF(DAY, MIN(ER.RetailDate), GETDATE()) 最早销售时间
         FROM ErpRetail ER 
         LEFT JOIN ErpRetailGoods ERG ON ER.RetailID = ERG.RetailID
         LEFT JOIN ErpRetailGoodsDetail ERGD ON ERG.RetailGoodsID=ERGD.RetailGoodsID
         LEFT JOIN ErpBaseGoodsSize EBGS ON ERGD.SizeId=EBGS.SizeId
         JOIN T1 ON ER.CustomerId=T1.CustomerId AND ERG.GoodsId=T1.GoodsId
         GROUP BY
-            ER.CustomerId,
-            ERG.GoodsId
+          ER.CustomerId,
+          ERG.GoodsId
         )
         
         SELECT 
-            EC.State,
-            EC.CustomerName,
-            EG.TimeCategoryName1,
-            EG.TimeCategoryName2,
-            EG.CategoryName1,
-            EG.CategoryName2,
-            EG.CategoryName,
-            EG.StyleCategoryName,
-            EG.GoodsNo,
-            EGPT.UnitPrice,
-            ISNULL(T.Price,EGPT.UnitPrice) 当前零售价,
-            T1.[调入数量_00/28/37/44/100/160/S],
+          EC.State,
+          EC.CustomerName,
+          EG.TimeCategoryName1,
+          EG.TimeCategoryName2,
+          EG.CategoryName1,
+          EG.CategoryName2,
+          EG.CategoryName,
+          EG.StyleCategoryName,
+          EG.GoodsNo,
+          EGPT.UnitPrice,
+          ISNULL(T.Price,EGPT.UnitPrice) 当前零售价,
+          T1.[调入数量_00/28/37/44/100/160/S],
           T1.[调入数量_29/38/46/105/165/M],
           T1.[调入数量_30/39/48/110/170/L],
           T1.[调入数量_31/40/50/115/175/XL],
@@ -245,8 +268,8 @@ class Ww_data_diaobo extends Command
           T1.[调入数量_36/6XL],
           T1.[调入数量_38/7XL],
           T1.[调入数量_40],
-            T1.调入总数量,
-            T2.[调入前库存_00/28/37/44/100/160/S],
+          T1.调入总数量,
+          T2.[调入前库存_00/28/37/44/100/160/S],
           T2.[调入前库存_29/38/46/105/165/M],
           T2.[调入前库存_30/39/48/110/170/L],
           T2.[调入前库存_31/40/50/115/175/XL],
@@ -257,8 +280,8 @@ class Ww_data_diaobo extends Command
           T2.[调入前库存_36/6XL],
           T2.[调入前库存_38/7XL],
           T2.[调入前库存_40],
-            T2.调入前库存,
-            T3.[调入后库存_00/28/37/44/100/160/S],
+          T2.调入前库存,
+          T3.[调入后库存_00/28/37/44/100/160/S],
           T3.[调入后库存_29/38/46/105/165/M],
           T3.[调入后库存_30/39/48/110/170/L],
           T3.[调入后库存_31/40/50/115/175/XL],
@@ -269,10 +292,10 @@ class Ww_data_diaobo extends Command
           T3.[调入后库存_36/6XL],
           T3.[调入后库存_38/7XL],
           T3.[调入后库存_40],
-            T3.调入后库存,
-            T2.[上市天数],
-            T4.[最早销售时间],
-            T4.[调拨前两周销量_00/28/37/44/100/160/S],
+          T3.调入后库存,
+          T2.[上市天数],
+          T4.[最早销售时间],
+          T4.[调拨前两周销量_00/28/37/44/100/160/S],
           T4.[调拨前两周销量_29/38/46/105/165/M],
           T4.[调拨前两周销量_30/39/48/110/170/L],
           T4.[调拨前两周销量_31/40/50/115/175/XL],
