a933a5012278ff3194bbc4b747ac49b3fd7b153b
chenweiliang <512430@qq.com>
2023-07-29 18:04:08 +0800

    66666

--- config/database.php
+++ config/database.php
@@ -102,13 +102,13 @@ return [
             'type'              => Env::get('database2.type', 'mysql'),
             // 服务器地址
             // 'hostname'          => Env::get('database2.hostname', '192.168.98.7'),
-            'hostname'          => Env::get('database2.hostname', '192.168.98.222'),
+            'hostname'          => Env::get('database2.hostname', '183.6.86.105'),
             // 数据库名
-            'database'          => Env::get('database2.database', 'bi2023'),
+            'database'          => Env::get('database2.database', 'bi'),
             // 用户名
-            'username'          => Env::get('database2.username', 'bi2023'),
+            'username'          => Env::get('database2.username', 'bi'),
             // 密码
-            'password'          => Env::get('database2.password', 'sokobi2023'),
+            'password'          => Env::get('database2.password', 'WAwFEb772YXDYAza'),
             // 端口
             'hostport'          => Env::get('database2.hostport', '3306'),
             // 数据库连接参数
