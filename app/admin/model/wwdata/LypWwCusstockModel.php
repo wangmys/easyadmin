9be9eed4485209cc7008dffd4acbcec019c29379
lyp <1606038679@qq.com>
2023-07-31 12:07:00 +0800

    ww数据

---
+++ app/admin/model/wwdata/LypWwCusstockModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\admin\model\wwdata;
+
+
+use app\common\model\TimeModel;
+
+class LypWwCusstockModel extends TimeModel
+{
+    // 表名
+    protected $name = 'lyp_ww_cusstock';
+}
--- app/api/controller/stock/Wwdata.php
+++ app/api/controller/stock/Wwdata.php
@@ -6,24 +6,16 @@ use app\api\constants\ApiConstant;
 use app\BaseController;
 use think\Request;
 use think\facade\Db;
-use app\admin\model\wwdata\LypWwDataModel;
-use app\admin\model\wwdata\LypWwDiaoboModel;
+use app\admin\model\wwdata\LypWwCusstockModel;
 
 class Wwdata extends BaseController
 {
 
-    public function sjp_customer_stock_json() {
+    public function ea_lyp_ww_cusstock() {
 
-        $bi_db = Db::connect("mysql2");
-        $data = $bi_db->Query("select cs.店铺名称, cs.季节, g.一级时间分类, CASE 
-        WHEN g.二级分类='短T' and (cs.当前零售价/cs.零售价)<1 and cs.当前零售价<=50 THEN '引流款' 
-        WHEN right(g.二级分类, 2)='长裤' and (cs.当前零售价/cs.零售价)<1 and cs.当前零售价<=100 THEN '引流款' 
-        WHEN right(g.二级分类, 2)='短裤' and (cs.当前零售价/cs.零售价)<1 and cs.当前零售价<=70 THEN '引流款' 
-        WHEN right(g.二级分类, 1)='衬' and (cs.当前零售价/cs.零售价)<1 and cs.当前零售价<=80 THEN '引流款' ELSE g.风格 END AS 修改后风格, g.一级分类, g.二级分类, g.分类, sum(cs.合计) as 库存数量合计, count(g.货号) as SKC数 from sjp_customer_stock cs 
- left join sjp_goods g on cs.货号=g.货号 
- where cs.季节 like '%夏%' 
- group by g.分类;");
-        // print_r($data);die;
+        ini_set('memory_limit','500M');
+        $data = LypWwCusstockModel::where([])->withoutField(['id', 'create_time'], false)->select();
+        $data = $data ? $data->toArray() : [];
         return json($data);
 
     }
