0c0a0b7516e4461926e34f54e8fc2883a172cf3c
chenweiliang <512430@qq.com>
2023-08-07 20:32:29 +0800

    66666

--- app/api/controller/lufei/Shangguitips.php
+++ app/api/controller/lufei/Shangguitips.php
@@ -191,4 +191,159 @@ class Shangguitips extends BaseController
         }
     }
 
+    public function changku()
+    {
+        $sql = "
+            SELECT 
+                m.*, 
+                m.上柜家数 / m.店铺个数 AS 上柜率,
+                date_format(now(),'%Y-%m-%d') AS 更新日期
+            FROM
+            (
+                SELECT
+                    sk.云仓,sk.年份,sk.季节,sk.一级分类,sk.二级分类,sk.分类,
+                    sk.货号,
+                    count(sk.预计库存数量) as 上柜家数,
+                    zysgs.直营上柜数,
+                    jmsgs.加盟上柜数,
+                    dpgs.店铺个数
+                FROM
+                    sp_sk as sk
+                LEFT JOIN (
+                    SELECT
+                        云仓,一级分类,二级分类,分类,货号,
+                        预计库存数量,count(预计库存数量) as 直营上柜数
+                    FROM
+                        sp_sk 
+                    WHERE 1
+                        AND 预计库存数量 > 0
+                        AND 经营模式 = '直营'
+                    GROUP BY
+                        云仓,季节,一级分类,二级分类,分类,货号
+                ) AS zysgs ON zysgs.云仓 = sk.云仓 AND zysgs.一级分类 = sk.一级分类 AND zysgs.二级分类 = sk.二级分类 AND zysgs.货号 = sk.货号
+                LEFT JOIN (
+                    SELECT
+                        云仓,一级分类,二级分类,分类,货号,
+                        预计库存数量,count(预计库存数量) as 加盟上柜数
+                    FROM
+                        sp_sk 
+                    WHERE 1
+                        AND 预计库存数量 > 0
+                        AND 经营模式 = '加盟'
+                    GROUP BY
+                        云仓,季节,一级分类,二级分类,分类,货号
+                ) AS jmsgs ON jmsgs.云仓 = sk.云仓 AND jmsgs.一级分类 = sk.一级分类 AND jmsgs.二级分类 = sk.二级分类 AND jmsgs.货号 = sk.货号
+                RIGHT JOIN (
+                    select t.云仓,count(*) AS 店铺个数 from 
+                    (	
+                        SELECT
+                            云仓,店铺名称
+                        FROM
+                            sp_sk 
+                        WHERE 1
+                        GROUP BY
+                            云仓,店铺名称
+                    ) as t
+                    GROUP BY t.云仓
+                ) AS dpgs ON sk.云仓 = dpgs.云仓
+                WHERE 1
+                    AND sk.预计库存数量 > 0
+                GROUP BY
+                    sk.云仓,sk.季节,sk.一级分类,sk.二级分类,sk.分类,sk.货号
+            ) AS m		
+        ";
+		
+        $select = $this->db_easyA->query($sql);
+        $count = count($select);
+
+        if ($select) {
+            // 删除历史数据
+            $this->db_easyA->execute('TRUNCATE cwl_shangguitips_cangku;');
+            $chunk_list = array_chunk($select, 500);
+
+
+            foreach($chunk_list as $key => $val) {
+                $this->db_easyA->table('cwl_shangguitips_cangku')->strict(false)->insertAll($val);
+            }
+
+            // 更新销售占比
+            // $sql2 = "
+            //     UPDATE cwl_shangguitips_retail AS r
+            //     RIGHT JOIN
+            //     (
+            //         SELECT 
+            //             季节归集,风格, sum(销售金额) as 销售总金额 FROM `cwl_shangguitips_retail` where 季节归集 = '春季' and 风格='基本款'
+            //         UNION ALL
+            //         SELECT 
+            //             季节归集,风格, sum(销售金额) as 销售总金额 FROM `cwl_shangguitips_retail` where 季节归集 = '春季' and 风格='引流款'
+            //         UNION ALL
+            //         SELECT 
+            //             季节归集,风格, sum(销售金额) as 销售总金额 FROM `cwl_shangguitips_retail` where 季节归集 = '夏季' and 风格='基本款'
+            //         UNION ALL
+            //         SELECT 
+            //             季节归集,风格, sum(销售金额) as 销售总金额 FROM `cwl_shangguitips_retail` where 季节归集 = '夏季' and 风格='引流款'
+            //         UNION ALL
+            //         SELECT 
+            //             季节归集,风格, sum(销售金额) as 销售总金额 FROM `cwl_shangguitips_retail` where 季节归集 = '秋季' and 风格='基本款'
+            //         UNION ALL
+            //         SELECT 
+            //             季节归集,风格, sum(销售金额) as 销售总金额 FROM `cwl_shangguitips_retail` where 季节归集 = '秋季' and 风格='引流款'
+            //         UNION ALL
+            //         SELECT 
+            //             季节归集,风格, sum(销售金额) as 销售总金额 FROM `cwl_shangguitips_retail` where 季节归集 = '冬季' and 风格='基本款'
+            //         UNION ALL
+            //         SELECT 
+            //             季节归集,风格, sum(销售金额) as 销售总金额 FROM `cwl_shangguitips_retail` where 季节归集 = '冬季' and 风格='引流款'
+            //     ) as t ON r.季节归集 = t.季节归集 AND r.风格 = t.风格
+            //     SET 
+            //         r.`销售总金额` = t.销售总金额,
+            //         r.`销售占比` = round(r.销售金额 / t.销售总金额, 3)
+            // ";
+            // $this->db_easyA->execute($sql2);
+
+            return json([
+                'status' => 1,
+                'msg' => 'success',
+                'content' => "cwl_shangguitips_cangku 更新成功，数量：{$count}！"
+            ]);
+
+        }
+    }
+
+    public function chagnku_2() {
+        // 更新风格 一级风格 二级风格
+        $sql1 = "
+            UPDATE
+                cwl_shangguitips_cangku AS ck 
+            LEFT JOIN sp_ww_budongxiao_yuncangkeyong AS ky ON  ck.一级分类 = ky.一级分类 AND ck.二级分类 = ky.二级分类 AND ck.分类 = ky.分类 AND ck.货号 = ky.货号
+            SET 
+                ck.风格 = ky.风格,
+                ck.一级风格 = ky.一级风格,
+                ck.二级风格 = ky.二级风格
+            WHERE 
+                1
+        ";
+
+        // 更新仓库可用
+        $sql2 = "
+            UPDATE
+                cwl_shangguitips_cangku AS ck 
+            LEFT JOIN sp_ww_budongxiao_yuncangkeyong AS ky ON ck.云仓 = ky.仓库名称 AND ck.一级分类 = ky.一级分类 AND ck.二级分类 = ky.二级分类 AND ck.分类 = ky.分类 AND ck.货号 = ky.货号
+            SET 
+                ck.`可用库存_00/28/37/44/100/160/S` = ky.`可用库存_00/28/37/44/100/160/S`,
+                ck.`可用库存_29/38/46/105/165/M` = ky.`可用库存_29/38/46/105/165/M`,
+                ck.`可用库存_30/39/48/110/170/L` = ky.`可用库存_30/39/48/110/170/L`,
+                ck.`可用库存_31/40/50/115/175/XL` = ky.`可用库存_31/40/50/115/175/XL`,
+                ck.`可用库存_32/41/52/120/180/2XL` = ky.`可用库存_32/41/52/120/180/2XL`,
+                ck.`可用库存_33/42/54/125/185/3XL` = ky.`可用库存_33/42/54/125/185/3XL`,
+                ck.`可用库存_34/43/56/190/4XL` = ky.`可用库存_34/43/56/190/4XL`,
+                ck.`可用库存_35/44/58/195/5XL` = ky.`可用库存_35/44/58/195/5XL`,
+                ck.`可用库存_36/6XL` = ky.`可用库存_36/6XL`,
+                ck.`可用库存_38/7XL` = ky.`可用库存_38/7XL`,
+                ck.`可用库存_40` = ky.`可用库存_40`,
+                ck.`可用数量` = ky.`可用库存Quantity`
+            WHERE 
+                1
+        ";
+    }
 }
