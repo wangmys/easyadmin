e61db6c19ec43f59a4129b543d4e9dd5e159ea7d
chenweiliang <512430@qq.com>
2023-09-01 00:00:59 +0800

    6666

--- app/admin/controller/system/Daxiaoma.php
+++ app/admin/controller/system/Daxiaoma.php
@@ -87,21 +87,66 @@ class Daxiaoma extends AdminController
                 if (!empty($input['商品负责人'])) {
                     // echo $input['商品负责人'];
                     $map1Str = xmSelectInput($input['商品负责人']);
-                    $map1 = " AND c.商品负责人 IN ({$map1Str})";
+                    // $map1 = " AND c.商品负责人 IN ({$map1Str})";
                 } else {
-                    $map1 = "";
+                    // $map1 = "";
                 }
             } else {
                 $admin = session('admin.name');
-                $map1 = " AND c.商品负责人 IN ('{$admin}')";
+                // $map1 = " AND c.商品负责人 IN ('{$admin}')";
             }
 
-            if (!empty($input['云仓'])) {
+            if (!empty($input['省份'])) {
                 // echo $input['商品负责人'];
-                $map2Str = xmSelectInput($input['云仓']);
-                $map2 = " AND c.云仓 IN ({$map2Str})";
+                $map1Str = xmSelectInput($input['省份']);
+                $map1 = " AND 省份 IN ({$map1Str})";
+            } else {
+                $map1 = "";
+            }
+
+            if (!empty($input['店铺名称'])) {
+                // echo $input['商品负责人'];
+                $map2Str = xmSelectInput($input['店铺名称']);
+                $map2 = " AND 店铺名称 IN ({$map2Str})";
             } else {
                 $map2 = "";
+            }
+
+            if (!empty($input['风格'])) {
+                // echo $input['商品负责人'];
+                $map3Str = xmSelectInput($input['风格']);
+                $map3 = " AND 风格 IN ({$map3Str})";
+            } else {
+                $map3 = "";
+            }
+
+            if (!empty($input['一级风格'])) {
+                // echo $input['商品负责人'];
+                $map4Str = xmSelectInput($input['一级风格']);
+                $map4 = " AND 一级风格 IN ({$map4Str})";
+            } else {
+                $map4 = "";
+            }
+
+            if (!empty($input['未上柜提醒'])) {
+                // echo $input['商品负责人'];
+                $exploadDate = explode(',', $input['未上柜提醒']);
+
+                $map5 = '';
+                foreach ($exploadDate as $key => $val) {
+                    $map5 .= " AND `". $val . "`='缺' ";
+                }
+                // echo $map5;
+            } else {
+                $map5 = "";
+            }
+
+            if (!empty($input['大小码提醒'])) {
+                // echo $input['商品负责人'];
+                $map6Str = xmSelectInput($input['大小码提醒']);
+                $map6 = " AND 大小码提醒 IN ({$map6Str})";
+            } else {
+                $map6 = "";
             }
 
             $sql = "
@@ -110,7 +155,13 @@ class Daxiaoma extends AdminController
                 FROM 
                     cwl_daxiao_handle
                 WHERE 1	
-                    AND 一级分类 IN ('内搭','外套')
+                    AND 一级分类 IN ('内搭')
+                    {$map1}
+                    {$map2}
+                    {$map3}
+                    {$map4}
+                    {$map5}
+                    {$map6}
                 LIMIT {$pageParams1}, {$pageParams2}  
             ";  
 
@@ -122,7 +173,13 @@ class Daxiaoma extends AdminController
                 FROM 
                     cwl_daxiao_handle
                 WHERE 1	
-                    AND 一级分类 IN ('内搭','外套')
+                    AND 一级分类 IN ('内搭')
+                    {$map1}
+                    {$map2}
+                    {$map3}
+                    {$map4}
+                    {$map5}
+                    {$map6}
             ";
             $count = $this->db_easyA->query($sql2);
             $find_config = $this->db_easyA->table('cwl_skauto_config')->where('id=1')->find();
@@ -137,20 +194,20 @@ class Daxiaoma extends AdminController
     // 获取筛选栏多选参数
     public function getXmMapSelect() {
         // 商品负责人
-        $customer17 = $this->db_easyA->query("
-            SELECT 商品负责人 as name, 商品负责人 as value FROM cwl_weathertips_customer WHERE 商品负责人 IS NOT NULL AND 商品负责人 !='0' GROUP BY 商品负责人
-        ");
+        // $customer17 = $this->db_easyA->query("
+        //     SELECT 商品负责人 as name, 商品负责人 as value FROM cwl_weathertips_customer WHERE 商品负责人 IS NOT NULL AND 商品负责人 !='0' GROUP BY 商品负责人
+        // ");
         $province = $this->db_easyA->query("
-            SELECT 省份 as name, 省份 as value FROM cwl_weathertips_customer WHERE 省份 IS NOT NULL GROUP BY 省份
+            SELECT 省份 as name, 省份 as value FROM cwl_daxiao_handle WHERE 省份 IS NOT NULL GROUP BY 省份
         ");
         $customer = $this->db_easyA->query("
-            SELECT 店铺名称 as name, 店铺名称 as value FROM cwl_weathertips_customer GROUP BY 店铺名称
+            SELECT 店铺名称 as name, 店铺名称 as value FROM cwl_daxiao_handle GROUP BY 店铺名称
         ");
         
         // 门店
         // $storeAll = SpWwBudongxiaoDetail::getMapStore();
 
-        return json(["code" => "0", "msg" => "", "data" => ['customer17' => $customer17, 'province' => $province, 'customer' => $customer]]);
+        return json(["code" => "0", "msg" => "", "data" => ['province' => $province, 'customer' => $customer]]);
     }
 
 
