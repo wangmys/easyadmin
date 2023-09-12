42624b2d74bab8045e466cdfa71f58f970d083b6
lyp <1606038679@qq.com>
2023-09-12 20:35:29 +0800

    自动铺货 满足率、动销率修改

--- public/static/admin/js/system/puhuo/scconfig.js
+++ public/static/admin/js/system/puhuo/scconfig.js
@@ -148,7 +148,145 @@ define(["jquery", "easy-admin", "vue"], function ($, ea, Vue) {
            });
             
             ea.listen();
-       }
+       },
+
+
+       //满足率评分标准
+       fill_rate:function (){
+            var fill_rate_head = $("#fill_rate_head").val();
+            var fill_rate_head = JSON.parse(fill_rate_head);
+            // console.log(fill_rate_head);
+            var html = '';
+            var that = this;
+            fill_rate_head.forEach(function (i,value) {
+                    html += '<td class="field_'+ value +'_'+ i +'"><input type="text" name="' + i + '[]" lay-verify="required" value="" placeholder="请输入" class="layui-input"></td>'
+            })
+            // $('.add_fill_rate').on('click', function(){
+            //     var element = $([
+            //         '<tr>',
+            //             ,html,
+            //             '<td class="handler">',
+            //                 '<input type="text" name="id[]" class="layui-hide" value="">',
+            //                 '<input type="text" name="config_str[]" class="layui-hide" value="customer_level">',
+            //                 '<input type="text" name="key_level[]" class="layui-hide" value="">',
+            //                 '<button type="button" class="layui-btn layui-btn-normal get" id="">保存</button>',
+            //                 '<button type="button" class="layui-btn layui-btn-danger del">删除</button>',
+            //             '</td>',
+            //         '</tr>',
+            //     ].join(''))
+            //         // console.log(html)
+                
+            //         element.find('.get').on('click', function(){
+            //             var _url = url.save_url;
+
+            //             var key = element.find('input[name="key[]"]').val();
+            //             var score = element.find('input[name="score[]"]').val();
+            //             var config_str = element.find('input[name="config_str[]"]').val();
+            //             var key_level = element.find('input[name="key_level[]"]').val();
+
+            //             var id = element.find('input[name="id[]"]').val();
+            //             var _data = {
+            //                 key:key,
+            //                 score:score,
+            //                 config_str:config_str,
+            //                 key_level:key_level,
+            //                 id:id
+            //             }
+            //             // 保存配置
+            //             that.saveConfig(element,_url,_data);
+            //         })
+
+            //         element.find('.del').on('click', function(){
+            //         var _this = $(this);
+            //         layer.confirm('是否删除该店铺等级',{},function () {
+            //                 var id = element.find('input[name="id[]"]').val();
+            //                 if (id != '') {
+            //                     that.delConfig(_this, url.del_url, id)
+            //                 }
+            //             $(_this).parents('tr').remove();
+            //             layer.closeAll()
+            //         })
+            //     })
+            //     $('#form-customer_level-config tbody').append(element);
+            // });
+
+                // 获取渲染后端渲染的数据,重新绑定事件
+                $.each($('.puhuo_fill_rate-select'), function (key, element) {
+
+                    $(element).find('.get_fill_rate').on('click', function(){
+                        
+                        var _url = url.save_url;
+                        
+                        var key = $(element).find('input[name="key[]"]').val();
+                        var score = $(element).find('input[name="score[]"]').val();
+                        var config_str = $(element).find('input[name="config_str[]"]').val();
+                        var key_level = $(element).find('input[name="key_level[]"]').val();
+
+                        var id = $(element).find('input[name="id[]"]').val();
+
+                        var _data = {
+                            key:key,
+                            score:score,
+                            config_str:config_str,
+                            key_level:key_level,
+                            id:id
+                        }
+                        // 保存配置
+                        that.saveConfig($(element), _url, _data);
+                    })
+
+                // $(element).find('.del').on('click', function(){
+                //     var _this = $(this);
+                //     var _id = $(element).find('input[name="id[]"]').val();
+                //     layer.confirm('是否删除该店铺等级',{},function () {
+                //             that.delConfig(_this, url.del_url, _id)
+                //     })
+                // })
+
+            });
+                
+            ea.listen();
+        },
+
+
+
+        //动销率评分标准
+        dongxiao_rate:function (){
+
+            var that = this;
+
+            // 获取渲染后端渲染的数据,重新绑定事件
+            $.each($('.puhuo_dongxiao_rate-select'), function (key, element) {
+
+                $(element).find('.get_dongxiao_rate').on('click', function(){
+                    
+                    var _url = url.save_url;
+                    
+                    var key = $(element).find('input[name="key[]"]').val();
+                    var score = $(element).find('input[name="score[]"]').val();
+                    var config_str = $(element).find('input[name="config_str[]"]').val();
+                    var key_level = $(element).find('input[name="key_level[]"]').val();
+
+                    var id = $(element).find('input[name="id[]"]').val();
+
+                    var _data = {
+                        key:key,
+                        score:score,
+                        config_str:config_str,
+                        key_level:key_level,
+                        id:id
+                    }
+                    // 保存配置
+                    that.saveConfig($(element), _url, _data);
+                })
+
+            });
+                
+            ea.listen();
+        }
+
+
+
 
 
     };
