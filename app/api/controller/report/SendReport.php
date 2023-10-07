c97207994ec28ed51ab32b3dfd650cf0be33a509
chenweiliang <512430@qq.com>
2023-10-07 13:24:28 +0800

    666

--- app/api/controller/report/SendReport.php
+++ app/api/controller/report/SendReport.php
@@ -976,17 +976,17 @@ class SendReport extends BaseController
         $time = time();
         $find = $this->db_easyA->table('dd_baobiao')->field('状态,可推送时间范围')->where(['id' => '2', '编号' => 's101'])->find();
         $可推送时间范围 = explode('-', $find['可推送时间范围']);
-        echo input('get.date');
-        echo '<br>';
-        echo input('get.user');
-        echo '<br>';
-        echo $find['状态'];
-        echo '<br>';
-        echo $可推送时间范围[0];
-        echo '<br>';
-        echo $可推送时间范围[1];
+        // echo input('get.date');
+        // echo '<br>';
+        // echo input('get.user');
+        // echo '<br>';
+        // echo $find['状态'];
+        // echo '<br>';
+        // echo $可推送时间范围[0];
+        // echo '<br>';
+        // echo $可推送时间范围[1];
 
-        die;
+        // die;
         if ( ($find && $find['状态'] == '开' && ( $time >= strtotime($可推送时间范围[0]) && $time <= strtotime($可推送时间范围[1]))) || input('user') == 'cwl' ) {
             // 需要手动传日期的话传查询日期+1天
             $date = input('date') ? input('date') : date('Y-m-d', strtotime('+1day'));
@@ -1003,15 +1003,13 @@ class SendReport extends BaseController
             $res = [];
             $headers = get_headers($send_data['jpg_url']);
             if(substr($headers[0], 9, 3) == 200){
-                echo $send_data['jpg_url'];
+                // echo $send_data['jpg_url'];
                 // 推送
                 // $res[] = $model->send($send_data['title'], $send_data['jpg_url']);
-                // $res[] = $model->send($send_data['title'],$send_data['jpg_url'], "https://oapi.dingtalk.com/robot/send?access_token=5091c1eb2c0f4593d79825856f26bc30dcb5f64722c3909e6909a1255630f8a2");
+                $res[] = $model->send($send_data['title'],$send_data['jpg_url'], "https://oapi.dingtalk.com/robot/send?access_token=5091c1eb2c0f4593d79825856f26bc30dcb5f64722c3909e6909a1255630f8a2");
             }
 
             return json($res);
-        } else {
-            echo '不可推送';
         }
     }
 
