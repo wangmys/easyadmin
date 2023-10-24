32d0cb73ec4267085e0998c64577581720a44ff6
chenweiliang <512430@qq.com>
2023-10-24 11:34:31 +0800

    6666

--- app/command/Cwl_caigou.php
+++ app/command/Cwl_caigou.php
@@ -45,7 +45,7 @@ class Cwl_caigou extends Command
                 // echo "wkhtmltoimage  --encoding utf-8 http://im.babiboy.com/admin/system.Caigou/zdt1?{$val['列']}={$val['值']} {$path}";die;
                 // wkhtmltoimage --encoding utf-8 http://im.babiboy.com/admin/system.Caigou/zdt1?中类=羽绒服 /data/web/cwl/cgzdt_test1.jpg
 
-                $res = system("wkhtmltoimage  --encoding utf-8 http://im.babiboy.com/admin/system.Caigou/zdt1?{$val['列']}={$val['值']} {$path}", $result);
+                // $res = system("wkhtmltoimage  --encoding utf-8 http://im.babiboy.com/admin/system.Caigou/zdt1?{$val['列']}={$val['值']} {$path}", $result);
                 // print $result;//输出命令的结果状态码
                 // print $res;//输出命令输出的最后一行
             }
