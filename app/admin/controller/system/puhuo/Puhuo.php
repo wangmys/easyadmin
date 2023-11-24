2c9a15578b0e0b4aa806001458813980ddd4ea2c
lyp <1606038679@qq.com>
2023-11-24 14:28:57 +0800

    excel转换需求

--- app/admin/controller/system/puhuo/Puhuo.php
+++ app/admin/controller/system/puhuo/Puhuo.php
@@ -5,6 +5,8 @@ use EasyAdmin\annotation\ControllerAnnotation;
 use EasyAdmin\annotation\NodeAnotation;
 use app\common\controller\AdminController;
 use app\admin\service\PuhuoService;
+use app\admin\model\bi\SpLypPuhuoWaitGoodsModel;
+use app\admin\model\CustomerModel;
 use jianyan\excel\Excel;
 use think\Request;
 
@@ -373,6 +375,150 @@ class Puhuo extends AdminController
         } else {
             return View('system/puhuo/caogao_index', ['setTime1' => date('Y-m-d'), 'setTime2' => date('Y-m-d')]);
         }        
+
+    }
+
+    /**
+     * @NodeAnotation(title="excel转换")
+     */
+    public function import_excel() {
+
+        if (request()->isAjax()) {
+            $file = request()->file('file');
+
+            $new_name = "import_excel" . '_' . uuid('zhuanhuan') . '.' . $file->getOriginalExtension();
+            $save_path = app()->getRootPath() . 'runtime/uploads/'.date('Ymd',time()).'/';   //文件保存路径
+            $info = $file->move($save_path, $new_name);
+
+            if ($info) {
+
+                return json(['code' => 0, 'msg'=>'导入成功', 'data'=>$save_path.$new_name]);
+
+            } else {
+
+                return json(['code' => 500, 'msg'=>'error,请联系系统管理员']);
+
+            }
+
+        } else {
+
+            $params = $this->request->param();
+            $excel_url = $params['excel_url'] ?? '';
+            if (!$excel_url) {
+                return json(['code' => 400, 'msg'=>'error,excel文件不存在']);
+            }
+
+            //读取导入的需要转单的excel
+            $read_column = [
+
+                'A' => '单号',
+                'B' => '云仓',
+                'C' => '货号',
+                'D' => '店铺',
+                'E' => '44/28/',
+                'F' => '46/29/165/38/M/105',
+                'G' => '48/30/170/39/L/110',
+                'H' => '50/31/175/40/XL/115',
+                'I' => '52/32/180/41/2XL/120',
+                'J' => '54/33/185/42/3XL/125',
+                'K' => '56/34/190/43/4XL/130',
+                'L' => '58/35/195/44/5XL/',
+                'M' => '60/36/6XL/',
+                'N' => '38/7XL',
+                'O' => '40',
+                'P' => '42',
+                'Q' => '合计',
+
+            ];
+
+            $res = importExcel($excel_url, $read_column);
+            // print_r($res);die;
+
+            if ($res) {
+
+                $excel_output_data = []; 
+                foreach ($res as $k_res=>$v_res) {
+
+                    // $order_no = uuid('SX');//date('Ymd').(++$k_res);
+
+                    //查询云仓-货号 对应数据
+                    $wait_goods_info = SpLypPuhuoWaitGoodsModel::where([['WarehouseName', '=', $v_res['云仓']], ['GoodsNo', '=', $v_res['货号']]])->find();
+                    $wait_goods_info = $wait_goods_info ? $wait_goods_info->toArray() : [];
+
+                    $customer_code = CustomerModel::where([['CustomerName', '=', $v_res['店铺']]])->field('CustomerCode')->find();
+                    $customer_code = $customer_code ? $customer_code->toArray() : [];
+                    // print_r([$wait_goods_info, $customer_code]);die;
+
+                    $tmp_arr = [
+                        'order_no' => $v_res['单号'],
+                        'WarehouseCode' => $this->return_ware_code($v_res['云仓']),
+                        'CustomerCode' => $customer_code ? $customer_code['CustomerCode'] : '',
+                        'send_out' => 'Y',
+                        're_confirm' => 'Y',
+                        'GoodsNo' => $v_res['货号'],
+                        'Size' => '',//
+                        'ColorCode' => $wait_goods_info ? $wait_goods_info['ColorCode'] : '',
+                        'puhuo_num' => 0,//
+                        'status' => 2,
+                        'remark' => $wait_goods_info ? $wait_goods_info['CategoryName1'] : '',
+                    ];
+
+                    $size_arr = [
+                        ['puhuo_num'=>$v_res['44/28/'], 'Size'=>$wait_goods_info['Stock_00_size']],
+                        ['puhuo_num'=>$v_res['46/29/165/38/M/105'], 'Size'=>$wait_goods_info['Stock_29_size']],
+                        ['puhuo_num'=>$v_res['48/30/170/39/L/110'], 'Size'=>$wait_goods_info['Stock_30_size']],
+                        ['puhuo_num'=>$v_res['50/31/175/40/XL/115'], 'Size'=>$wait_goods_info['Stock_31_size']],
+                        ['puhuo_num'=>$v_res['52/32/180/41/2XL/120'], 'Size'=>$wait_goods_info['Stock_32_size']],
+                        ['puhuo_num'=>$v_res['54/33/185/42/3XL/125'], 'Size'=>$wait_goods_info['Stock_33_size']],
+                        ['puhuo_num'=>$v_res['56/34/190/43/4XL/130'], 'Size'=>$wait_goods_info['Stock_34_size']],
+                        ['puhuo_num'=>$v_res['58/35/195/44/5XL/'], 'Size'=>$wait_goods_info['Stock_35_size']],
+                        ['puhuo_num'=>$v_res['60/36/6XL/'], 'Size'=>$wait_goods_info['Stock_36_size']],
+                        ['puhuo_num'=>$v_res['38/7XL'], 'Size'=>$wait_goods_info['Stock_38_size']],
+                        ['puhuo_num'=>$v_res['40'], 'Size'=>$wait_goods_info['Stock_40_size']],
+                        ['puhuo_num'=>$v_res['42'], 'Size'=>$wait_goods_info['Stock_42_size']],
+                    ];
+                    foreach ($size_arr as $k_size_arr=>&$v_size_arr) {
+                        if ($v_size_arr['puhuo_num']) {
+                            $tmp_arr['Size'] = $v_size_arr['Size'];
+                            $tmp_arr['puhuo_num'] = $v_size_arr['puhuo_num'];
+                            $excel_output_data[] = $tmp_arr;
+                        }
+                    }
+
+                }
+
+                $header = [
+                    ['*订单号', 'order_no'],
+                    ['*仓库编号', 'WarehouseCode'],
+                    ['*店铺编号', 'CustomerCode'],
+                    ['*打包后立即发出', 'send_out'],
+                    ['*差异出货需二次确认', 're_confirm'],
+                    ['*货号', 'GoodsNo'],
+                    ['*尺码', 'Size'],
+                    ['*颜色编码', 'ColorCode'],
+                    ['*铺货数', 'puhuo_num'],
+                    ['*状态/1草稿,2预发布,3确定发布', 'status'],
+                    ['备注', 'remark'],
+                ];
+    
+                return Excel::exportData($excel_output_data, $header, 'import_excel_zhuanhuan' .count($excel_output_data) , 'xlsx');
+
+            }
+
+        }
+
+    }
+
+    public function return_ware_code($yuncang) {
+
+        $arr = [
+            '长沙云仓' => 'CK006',
+            '武汉云仓' => 'CK003',
+            '南昌云仓' => 'CK002',
+            '广州云仓' => 'CK004',
+            '贵阳云仓' => 'CK005',
+        ];
+        return $arr[$yuncang] ?? '';
 
     }
 
