7f709b9cb9d7d5cefcb399335fe62cc2e8e78411
chenweiliang <512430@qq.com>
2023-11-24 20:44:23 +0800

    666

--- app/admin/controller/system/Daxiaoma.php
+++ app/admin/controller/system/Daxiaoma.php
@@ -265,136 +265,6 @@ class Daxiaoma extends AdminController
         ]);
     }
 
-    /**
-     * @NodeAnotation(title="大小码提醒-内搭") 
-     * 
-     */
-    // public function handle_neida() {
-    //     $find_config = $this->db_easyA->table('cwl_weathertips_config')->where('id=1')->find();
-    //     if (request()->isAjax()) {
-    //         // 筛选条件
-    //         $input = input();
-    //         $pageParams1 = ($input['page'] - 1) * $input['limit'];
-    //         $pageParams2 = input('limit');
-
-    //         foreach ($input as $key => $val) {
-    //             // echo $val;
-    //             if ( $key != '在途库存') {
-    //                 if (empty($val)) {
-    //                     unset($input[$key]);
-    //                 }
-    //             }
-    //         }
-
-    //         // dump($input);die;
-    //         if (checkAdmin()) {
-    //             if (!empty($input['商品负责人'])) {
-    //                 // echo $input['商品负责人'];
-    //                 $map1Str = xmSelectInput($input['商品负责人']);
-    //                 // $map1 = " AND c.商品负责人 IN ({$map1Str})";
-    //             } else {
-    //                 // $map1 = "";
-    //             }
-    //         } else {
-    //             $admin = session('admin.name');
-    //             // $map1 = " AND c.商品负责人 IN ('{$admin}')";
-    //         }
-
-    //         if (!empty($input['省份'])) {
-    //             // echo $input['商品负责人'];
-    //             $map1Str = xmSelectInput($input['省份']);
-    //             $map1 = " AND 省份 IN ({$map1Str})";
-    //         } else {
-    //             $map1 = "";
-    //         }
-
-    //         if (!empty($input['店铺名称'])) {
-    //             // echo $input['商品负责人'];
-    //             $map2Str = xmSelectInput($input['店铺名称']);
-    //             $map2 = " AND 店铺名称 IN ({$map2Str})";
-    //         } else {
-    //             $map2 = "";
-    //         }
-
-    //         if (!empty($input['风格'])) {
-    //             // echo $input['商品负责人'];
-    //             $map3Str = xmSelectInput($input['风格']);
-    //             $map3 = " AND 风格 IN ({$map3Str})";
-    //         } else {
-    //             $map3 = "";
-    //         }
-
-    //         if (!empty($input['一级风格'])) {
-    //             // echo $input['商品负责人'];
-    //             $map4Str = xmSelectInput($input['一级风格']);
-    //             $map4 = " AND 一级风格 IN ({$map4Str})";
-    //         } else {
-    //             $map4 = "";
-    //         }
-
-    //         if (!empty($input['未上柜提醒'])) {
-    //             // echo $input['商品负责人'];
-    //             $exploadDate = explode(',', $input['未上柜提醒']);
-
-    //             $map5 = '';
-    //             foreach ($exploadDate as $key => $val) {
-    //                 $map5 .= " AND `". $val . "`='缺' ";
-    //             }
-    //             // echo $map5;
-    //         } else {
-    //             $map5 = "";
-    //         }
-
-    //         if (!empty($input['大小码提醒'])) {
-    //             // echo $input['商品负责人'];
-    //             $map6Str = xmSelectInput($input['大小码提醒']);
-    //             $map6 = " AND 大小码提醒 IN ({$map6Str})";
-    //         } else {
-    //             $map6 = "";
-    //         }
-
-    //         $sql = "
-    //             SELECT 
-    //                 * 
-    //             FROM 
-    //                 cwl_daxiao_handle
-    //             WHERE 1	
-    //                 AND 一级分类 IN ('内搭')
-    //                 {$map1}
-    //                 {$map2}
-    //                 {$map3}
-    //                 {$map4}
-    //                 {$map5}
-    //                 {$map6}
-    //             LIMIT {$pageParams1}, {$pageParams2}  
-    //         ";  
-
-    //         $select = $this->db_easyA->query($sql);
-
-    //         $sql2 = "
-    //             SELECT 
-    //                 count(*) as total
-    //             FROM 
-    //                 cwl_daxiao_handle
-    //             WHERE 1	
-    //                 AND 一级分类 IN ('内搭')
-    //                 {$map1}
-    //                 {$map2}
-    //                 {$map3}
-    //                 {$map4}
-    //                 {$map5}
-    //                 {$map6}
-    //         ";
-    //         $count = $this->db_easyA->query($sql2);
-    //         $find_config = $this->db_easyA->table('cwl_skauto_config')->where('id=1')->find();
-    //         return json(["code" => "0", "msg" => "", "count" => $count[0]['total'], "data" => $select]);
-    //     } else {
-    //         return View('handle_neida', [
-
-    //         ]);
-    //     }
-    // }
-
     // 获取筛选栏多选参数
     public function getXmMapSelect() {
         // 商品负责人
@@ -414,5 +284,163 @@ class Daxiaoma extends AdminController
         return json(["code" => "0", "msg" => "", "data" => ['province' => $province, 'customer' => $customer]]);
     }
 
+    public function excel_handle() {
+        $find_config = $this->db_easyA->table('cwl_weathertips_config')->where('id=1')->find();
+        if (request()->isAjax()) {
+        // if (1) {
+            // 筛选条件
+            $input = input();
 
+            foreach ($input as $key => $val) {
+                // echo $val;
+                if ( $key != '在途库存') {
+                    if (empty($val)) {
+                        unset($input[$key]);
+                    }
+                }
+            }
+
+            // dump($input);die;
+            if (checkAdmin()) {
+                if (!empty($input['商品负责人'])) {
+                    // echo $input['商品负责人'];
+                    $map1Str = xmSelectInput($input['商品负责人']);
+                    // $map1 = " AND c.商品负责人 IN ({$map1Str})";
+                } else {
+                    // $map1 = "";
+                }
+            } else {
+                $admin = session('admin.name');
+                // $map1 = " AND c.商品负责人 IN ('{$admin}')";
+            }
+
+            if (!empty($input['类型'])) {
+                if ($input['类型'] == '内搭' || $input['类型'] == '外套') {
+                    $map0 = " AND 一级分类 IN ('{$input['类型']}')";
+                } elseif ($input['类型'] == '下装') {
+                    $map0 = " AND 一级分类 IN ('下装') AND 二级分类 NOT IN ('松紧长裤', '松紧短裤')";
+                } elseif ($input['类型'] == '松紧') {
+                    $map0 = " AND 一级分类 IN ('下装') AND 二级分类 IN ('松紧长裤', '松紧短裤')";
+                } elseif ($input['类型'] == '鞋履') {
+                    $map0 = " AND 一级分类 IN ('鞋履')";
+                } else {
+                    die;
+                }
+            }
+
+            if (!empty($input['省份'])) {
+                // echo $input['商品负责人'];
+                $map1Str = xmSelectInput($input['省份']);
+                $map1 = " AND 省份 IN ({$map1Str})";
+            } else {
+                $map1 = "";
+            }
+
+            if (!empty($input['店铺名称'])) {
+                // echo $input['商品负责人'];
+                $map2Str = xmSelectInput($input['店铺名称']);
+                $map2 = " AND 店铺名称 IN ({$map2Str})";
+            } else {
+                $map2 = "";
+            }
+
+            if (!empty($input['风格'])) {
+                // echo $input['商品负责人'];
+                $map3Str = xmSelectInput($input['风格']);
+                $map3 = " AND 风格 IN ({$map3Str})";
+            } else {
+                $map3 = "";
+            }
+
+            if (!empty($input['一级风格'])) {
+                // echo $input['商品负责人'];
+                $map4Str = xmSelectInput($input['一级风格']);
+                $map4 = " AND 一级风格 IN ({$map4Str})";
+            } else {
+                $map4 = "";
+            }
+
+            if (!empty($input['未上柜提醒'])) {
+                // echo $input['商品负责人'];
+                $exploadDate = explode(',', $input['未上柜提醒']);
+
+                $map5 = '';
+                foreach ($exploadDate as $key => $val) {
+                    $map5 .= " AND `". $val . "`='缺' ";
+                }
+                // echo $map5;
+            } else {
+                $map5 = "";
+            }
+
+            if (!empty($input['大小码提醒'])) {
+                // echo $input['商品负责人'];
+                $map6Str = xmSelectInput($input['大小码提醒']);
+                $map6 = " AND 大小码提醒 IN ({$map6Str})";
+            } else {
+                $map6 = "";
+            }
+            
+            if (!empty($input['季节归集'])) {
+                // echo $input['商品负责人'];
+                $map7Str = xmSelectInput($input['季节归集']);
+                $map7 = " AND 季节归集 IN ({$map7Str})";
+            } else {
+                $map7 = "";
+            }
+
+            $map = "{$map0}{$map1}{$map2}{$map3}{$map4}{$map5}{$map6}{$map7}";
+
+            $code = rand_code(6);
+            cache($code, $map, 3600);
+            return json([
+                'status' => 1,
+                'code' => $code,
+            ]); 
+
+        } else {
+            $code = input('code');
+            $map = cache($code);
+            if (empty($map)) {
+                $map = '';
+            }
+
+            // dump($map);
+            // die;
+            $sql = "
+                SELECT 
+                    left(省份, 2) as 省份,
+                    店铺名称,一级分类,二级分类,
+                    left(风格, 2) as 风格,
+                    left(一级风格, 2) as 一级风格,
+                    季节归集,`预计SKC_00/28/37/44/100/160/S`,`预计SKC_29/38/46/105/165/M`,`预计SKC_30/39/48/110/170/L`,`预计SKC_31/40/50/115/175/XL`,
+                    `预计SKC_32/41/52/120/180/2XL`,`预计SKC_33/42/54/125/185/3XL`,`预计SKC_34/43/56/190/4XL`,`预计SKC_35/44/58/195/5XL`,`预计SKC_36/6XL`,`预计SKC_38/7XL`,`预计SKC_40`,
+                    `预计00/28/37/44/100/160/S`,`预计29/38/46/105/165/M`,`预计30/39/48/110/170/L`,`预计31/40/50/115/175/XL`,`预计32/41/52/120/180/2XL`,`预计33/42/54/125/185/3XL`,
+                    `预计34/43/56/190/4XL`,`预计35/44/58/195/5XL`,`预计36/6XL`,`预计38/7XL`,`预计_40`,`预计库存数量`,`总入量00/28/37/44/100/160/S`,`总入量29/38/46/105/165/M`,
+                    `总入量30/39/48/110/170/L`,`总入量31/40/50/115/175/XL`,`总入量32/41/52/120/180/2XL`,`总入量33/42/54/125/185/3XL`,`总入量34/43/56/190/4XL`,`总入量35/44/58/195/5XL`,
+                    `总入量36/6XL`,`总入量38/7XL`,`总入量_40`,`总入量数量`,`累销00/28/37/44/100/160/S`,`累销29/38/46/105/165/M`,`累销30/39/48/110/170/L`,`累销31/40/50/115/175/XL`,
+                    `累销32/41/52/120/180/2XL`,`累销33/42/54/125/185/3XL`,`累销34/43/56/190/4XL`,`累销35/44/58/195/5XL`,`累销36/6XL`,`累销38/7XL`,`累销_40`,`累销数量`,`店销占比00/28/37/44/100/160/S`,
+                    `店销占比29/38/46/105/165/M`,`店销占比30/39/48/110/170/L`,`店销占比31/40/50/115/175/XL`,`店销占比32/41/52/120/180/2XL`,`店销占比33/42/54/125/185/3XL`,`店销占比34/43/56/190/4XL`,
+                    `店销占比35/44/58/195/5XL`,`店销占比36/6XL`,`店销占比38/7XL`,`店销占比_40`,`省销占比00/28/37/44/100/160/S`,`省销占比29/38/46/105/165/M`,`省销占比30/39/48/110/170/L`,
+                    `省销占比31/40/50/115/175/XL`,`省销占比32/41/52/120/180/2XL`,`省销占比33/42/54/125/185/3XL`,`省销占比34/43/56/190/4XL`,`省销占比35/44/58/195/5XL`,`省销占比36/6XL`,`省销占比38/7XL`,
+                    `省销占比_40`,`店销占比排名1`,`店销占比排名2`,`店销占比排名3`,`店销占比排名4`,`店销占比排名5`,`店销占比排名6`,`未上柜提醒00/28/37/44/100/160/S`,`未上柜提醒29/38/46/105/165/M`,
+                    `未上柜提醒34/43/56/190/4XL`,`未上柜提醒35/44/58/195/5XL`,`未上柜提醒36/6XL`,`未上柜提醒38/7XL`,`未上柜提醒_40`,`大小码提醒`   
+                FROM 
+                    cwl_daxiao_handle
+                WHERE 1	
+                    {$map}
+            "; 
+            $select = $this->db_easyA->query($sql);
+            $header = [];
+            foreach($select[0] as $key => $val) {
+                $header[] = [$key, $key];
+            }
+            return Excel::exportData($select, $header, '门店无大小码提醒_' . input('类型') . '_'  . date('Ymd') . '_' . time() , 'xlsx');
+        }
+    } 
+
+    public function test() {
+        $map = cache('119732');
+        dump($map);
+    }
 }
--- app/admin/controller/system/Shangguitips.php
+++ app/admin/controller/system/Shangguitips.php
@@ -203,6 +203,223 @@ class Shangguitips extends AdminController
         }
     }
 
+    // 下载handle
+    public function excel_handle() {
+        if (request()->isAjax()) {
+            // 筛选条件
+            $input = input();
+
+            foreach ($input as $key => $val) {
+                // echo $val;
+                if ( $key != '在途库存') {
+                    if (empty($val)) {
+                        unset($input[$key]);
+                    }
+                }
+            }
+            if (!empty($input['云仓'])) {
+                // echo $input['商品负责人'];
+                $map1Str = xmSelectInput($input['云仓']);
+                $map1 = " AND 云仓 IN ({$map1Str})";
+            } else {
+                $map1 = "";
+            }
+            if (!empty($input['风格'])) {
+                // echo $input['商品负责人'];
+                $map2Str = xmSelectInput($input['风格']);
+                $map2 = " AND 风格 IN ({$map2Str})";
+            } else {
+                $map2 = "";
+            }   
+            if (!empty($input['货号'])) {
+                // echo $input['商品负责人'];
+                $map3Str = xmSelectInput($input['货号']);
+                $map3 = " AND 货号 IN ({$map3Str})";
+            } else {
+                $map3 = "";
+            }
+            
+            if (!empty($input['上柜提醒'])) {
+                // echo $input['商品负责人'];
+                $map4Str = xmSelectInput($input['上柜提醒']);
+                $map4 = " AND 上柜提醒 IN ({$map4Str})";
+            } else {
+                $map4 = "";
+            }
+
+            if (!empty($input['季节归集'])) {
+                // echo $input['商品负责人'];
+                $map5Str = xmSelectInput($input['季节归集']);
+                $map5 = " AND 季节归集 IN ({$map5Str})";
+            } else {
+                $map5 = "";
+            }
+
+            if (!empty($input['一级分类'])) {
+                // echo $input['商品负责人'];
+                $map6Str = xmSelectInput($input['一级分类']);
+                $map6 = " AND 一级分类 IN ({$map6Str})";
+            } else {
+                $map6 = "";
+            }
+
+            if (!empty($input['二级分类'])) {
+                // echo $input['商品负责人'];
+                $map7Str = xmSelectInput($input['二级分类']);
+                $map7 = " AND 二级分类 IN ({$map7Str})";
+            } else {
+                $map7 = "";
+            }
+
+            if (!empty($input['二级风格'])) {
+                // echo $input['商品负责人'];
+                $map8Str = xmSelectInput($input['二级风格']);
+                $map8 = " AND 二级风格 IN ({$map8Str})";
+            } else {
+                $map8 = "";
+            }
+
+            if (!empty($input['云仓_主码齐码情况'])) {
+                // echo $input['商品负责人'];
+                $map9Str = xmSelectInput($input['云仓_主码齐码情况']);
+                $map9 = " AND 云仓_主码齐码情况 IN ({$map9Str})";
+            } else {
+                $map9 = "";
+            }
+            $map = "{$map1}{$map2}{$map3}{$map4}{$map5}{$map6}{$map7}{$map8}{$map9}";
+            $code = rand_code(6);
+            cache($code, $map, 3600);
+            return json([
+                'status' => 1,
+                'code' => $code,
+            ]); 
+
+        } else {
+            $code = input('code');
+            $map = cache($code);
+            if (empty($map)) {
+                $map = '';
+            }
+            $sql = "
+                SELECT
+                    left(云仓, 2) as 云仓,
+                    季节归集,一级分类,二级分类,
+                    分类,风格,二级风格,货号,
+                    店铺个数_直营,店铺个数_加盟,店铺个数_合计,
+                    云仓_可用数量,云仓_主码齐码情况,
+                    实际上柜_直营上柜数,实际上柜_加盟上柜数,实际上柜_上柜家数,
+                    货品等级_计划_直营,货品等级_计划_加盟,货品等级_计划_合计,
+                    货品等级_实际_直营,货品等级_实际_加盟,货品等级_实际_合计,
+                    实际铺货_直营,实际铺货_加盟,实际铺货_合计,
+                    concat(round(铺货率_直营 * 100, 1), '%') as `铺货率_直营`,
+                    concat(round(铺货率_加盟 * 100, 1), '%') as 铺货率_加盟,
+                    concat(round(铺货率_合计 * 100, 1), '%') as 铺货率_合计,
+                    concat(round(上柜率_直营 * 100, 1), '%') as 上柜率_直营,
+                    concat(round(上柜率_加盟 * 100, 1), '%') as 上柜率_加盟,
+                    concat(round(上柜率_合计 * 100, 1), '%') as 上柜率_合计,
+                    concat(round(货品等级上柜率_直营 * 100, 1), '%') as 货品等级上柜率_直营,
+                    concat(round(货品等级上柜率_加盟 * 100, 1), '%') as 货品等级上柜率_加盟,
+                    concat(round(货品等级上柜率_合计 * 100, 1), '%') as 货品等级上柜率_合计,
+                    预计最大可加铺店数,单款全国日均销排名,
+                    concat(round(近1周中类销售占比 * 100, 1), '%') as 近1周中类销售占比,
+                    上柜提醒,克重,主码最小值,主码最小值码数,
+                    更新日期
+                FROM cwl_shangguitips_handle
+                WHERE 1	
+                    {$map} 
+                ORDER BY 
+                    云仓,季节归集 DESC,风格
+            ";  
+
+            $select = $this->db_easyA->query($sql);
+            $header = [];
+            foreach($select[0] as $key => $val) {
+                $header[] = [$key, $key];
+            }
+            return Excel::exportData($select, $header, '新品上柜提醒_' . date('Ymd') . '_' . time() , 'xlsx');
+        }
+    }    
+
+    // 下载可上
+    public function excel_keshang() {
+        if (request()->isAjax()) {
+            // 筛选条件
+            $input = input();
+            // $云仓 = $input['yc'];
+            // $货号 = $input['gdno'];
+            if (!empty($input['yc'])) {
+                $map1 = " AND `云仓` = '{$云仓}云仓'";                
+            } else {
+                $map1 = "";
+            }
+            if (!empty($input['gdno'])) {
+                $map2 = " AND `货号` = '{$货号}'";                
+            } else {
+                $map2 = "";
+            }
+
+            if (!empty($input['云仓'])) {
+                // echo $input['商品负责人'];
+                $map3Str = xmSelectInput($input['云仓']);
+                $map3 = " AND 云仓 IN ({$map3Str})";
+            } else {
+                $map3 = "";
+            }
+            if (!empty($input['经营模式'])) {
+                // echo $input['商品负责人'];
+                $map4Str = xmSelectInput($input['经营模式']);
+                $map4 = " AND 经营模式 IN ({$map4Str})";
+            } else {
+                $map4 = "";
+            }   
+            if (!empty($input['货号'])) {
+                // echo $input['商品负责人'];
+                $map5Str = xmSelectInput($input['货号']);
+                $map5 = " AND 货号 IN ({$map5Str})";
+            } else {
+                $map5 = "";
+            }
+            
+            if (!empty($input['店铺名称'])) {
+                // echo $input['商品负责人'];
+                $map6Str = xmSelectInput($input['店铺名称']);
+                $map6 = " AND 店铺名称 IN ({$map6Str})";
+            } else {
+                $map6 = "";
+            }
+
+            $map = "{$map1}{$map2}{$map3}{$map4}{$map5}{$map6}";
+            $code = rand_code(6);
+            cache($code, $map, 3600);
+            return json([
+                'status' => 1,
+                'code' => $code,
+            ]); 
+
+        } else {
+            $code = input('code');
+            $map = cache($code);
+            if (empty($map)) {
+                $map = '';
+            }
+            $sql = "
+                SELECT 
+                    *
+                    FROM cwl_shangguitips_keshang_customer
+                WHERE 1
+                    {$map}
+                ORDER BY
+                    云仓,货号,经营模式
+            ";
+            $select = $this->db_easyA->query($sql);
+            $header = [];
+            foreach($select[0] as $key => $val) {
+                $header[] = [$key, $key];
+            }
+            return Excel::exportData($select, $header, '可上店铺明细_' . date('Ymd') . '_' . time() , 'xlsx');
+        }
+    } 
+    
     public function handle2() {
         $find_config = $this->db_easyA->table('cwl_shangguitips_config')->where('id=1')->find();
         $keshang_url = $_SERVER['REQUEST_SCHEME'] . '://'. $_SERVER['HTTP_HOST'] . url('admin/system.Shangguitips/keshang_customer');
