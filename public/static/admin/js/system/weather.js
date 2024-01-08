2cc9aea3510dfffaac713c4a00b43072d5f0ddb8
M <1348580606@QQ.COM>
2024-01-08 19:47:47 +0800

    天气

--- public/static/admin/js/system/weather.js
+++ public/static/admin/js/system/weather.js
@@ -50,7 +50,6 @@ define(["jquery", "easy-admin2"], function ($, ea) {
                     {field: 'CustomItem17', width: 100, title: '商品负责人',fixed:'left',search: 'xmSelect',selectList:liable_list},
                     {hide:true, field: 'Mathod', width: 50, title: '经营模式',fixed:'left',search: 'xmSelect',selectList:mathod},
                     {hide:true, field: 'CustomerGrade', width: 50, title: '店铺等级',fixed:'left',search: 'xmSelect',selectList:CustomerGrade,laySearch:true},
-                    {hide:true, field: 'url_2345_cid', width: 70, title: '绑网址',fixed:'left',search: false,search: 'select',selectList:bang_url_list},
                     // {field: 'City', width: 100, title: '地级市',fixed:'left',search: 'select',selectList:city_list,laySearch:true},
                     // {field: 'BdCity', width: 90, title: '绑定城市',fixed:'left',search: false},
                     // {field: 'SendGoodsGroup', width: 150, title: '温度带',fixed:'left'},
@@ -77,21 +76,13 @@ define(["jquery", "easy-admin2"], function ($, ea) {
                 // console.log(cols);
 
                 cols.push({
-                    width: 120,
+                    width: 60,
                     title: '操作',
                     templet: ea.table.tool,
                     operat: [
                         [{
                             text: '绑城市',
                             url: init.city_url,
-                            method: 'open',
-                            auth: '',
-                            class: 'layui-btn layui-btn-normal layui-btn-xs',
-                            field:'CustomerId'
-                        }],
-                        [{
-                            text: '绑网址',
-                            url: init.tianqi_url,
                             method: 'open',
                             auth: '',
                             class: 'layui-btn layui-btn-normal layui-btn-xs',
